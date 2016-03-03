a515b24722380a4122aa11e24cfd9d55c9a36d85
neimad971 <damientalbot26@gmail.com>
2016-03-03 20:34:33 +0000

    Emailing Basics

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,13 @@
+class NotificationMailer < ActionMailer::Base
+  
+
+  default from: "no-reply@nomsterapp.com"
+
+
+   def comment_added
+     mail(to: "damientalbot26@gmail.com",
+          subject: "A comment has been added to your place")	
+   end
+
+
+end
