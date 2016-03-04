3a45f315ee9df9e7d9c38feeabaf46a0d16f13d5
neimad971 <damientalbot26@gmail.com>
2016-03-04 14:49:57 +0000

    Triggering the email when comments get added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,9 @@ class Comment < ActiveRecord::Base
   belongs_to :place
 
 
+  after_create :send_comment_email
+
+
   RATINGS = {
     'one star'    => '1_star',
     'two stars'   => '2_stars',
@@ -16,6 +19,11 @@ class Comment < ActiveRecord::Base
 
   def humanized_rating
     RATINGS.invert[self.rating]
+  end
+
+
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver
   end
 
 
